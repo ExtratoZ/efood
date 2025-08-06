da17f24a14d8c1ed98277c9a52543d451fa93b60
Caetano <cacamelo98@hotmail.com>
2025-08-06 14:57:23 -0300

    fix header perfil

--- src/components/HeaderPerfil/styles.ts
+++ src/components/HeaderPerfil/styles.ts
@@ -9,7 +9,7 @@ export const HeaderPerfilSection = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 60px;
+  padding: 60px 0px;
   width: 100%;
   position: relative;
 `
